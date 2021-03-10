f8fd4c0ea05df09bc773d666446265c6d7d66556
sumantoAcc <78916881+sumantoAcc@users.noreply.github.com>
2021-03-10 14:55:44 +0530

    Delete Frontend directory

--- Frontend/src/app/review.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-@Injectable({
-  providedIn: 'root',
-})
-export class ReviewService {
-  constructor(private http: HttpClient) { }
-}
